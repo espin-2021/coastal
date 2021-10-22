e18a322eea80c6e9f8f66201926c1bb33dbc38f8
connerlester <53120463+connerlester@users.noreply.github.com>
2021-10-22 18:12:25 -0400

    Add files via upload

--- runmodels_functions.py
+++ runmodels_functions.py
@@ -7,6 +7,10 @@ import numpy as np
 import pandas as pd
 from IPython.display import clear_output
 from datetime import datetime
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import importlib
+def reload(module_name):
+    importlib.reload(module_name)
 
 def shorelinetogrid(x, y, dx, dy, plotdata=True):
     """ function to convert xy shoreline to gridded elevation for input to CEM
@@ -48,7 +52,9 @@ def shorelinetogrid(x, y, dx, dy, plotdata=True):
     zg[IN] = (min(A * (dist[IN]) ** (2 / 3))) + 1
     zg = zg * -1
     if plotdata == True:
-        plt.figure(figsize=(10, 8))
+        M,N = zg.shape
+        s = M/N
+        plt.figure(figsize=(int(s*8),7))
         Bathy = plt.contourf(xg, yg, zg, cmap=plt.cm.GnBu)
         cbar = plt.colorbar(Bathy)
         cbar.ax.set_ylabel('Water Depth (m)', fontsize=20, rotation=-90, labelpad=30)
@@ -80,9 +86,12 @@ def plot_coast(domain,dx,dy):
     s = M/N
     fig,ax = plt.subplots(figsize=(int(s*8),7))
     im = ax.imshow(domain, origin='lower', cmap='viridis')
-    cb = fig.colorbar(im,ax=ax)
+    
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    cb = fig.colorbar(im,cax=cax)
+    cb.ax.tick_params('both',labelsize=15)
     cb.ax.set_ylabel('Water Depth (m)',fontsize=20,rotation=-90, labelpad=30)
-    cb.ax.tick_params('y',labelsize=15)
     y = np.linspace(0,N,4)
     x = np.linspace(0,M,4)
     Y = (y*dy/(1000)).astype('int')
@@ -126,10 +135,10 @@ def run_model_loop(time_years, domain ,cem ,waves, animate=True,update_ani_years
         cem.set_value(alpha, angle)
         cem.update()
         if animate:
-            if time%update_ani == 0:
+            if time%update_ani == 0 or time==T-1:
                 clear_output(wait=True)
                 plot_coast(cem.get_value('land_surface__elevation').reshape(domain.shape),dx,dy)
-                plt.title('Time : '+ str(int(time*cem.get_value('model__time_step')/365)) +' years',fontsize=20)
+                plt.title('Time : '+ str(round((time*cem.get_value('model__time_step')/365)[0],1)) +' years',fontsize=20)
                 plt.show()
 
         else:
@@ -165,12 +174,12 @@ def initialize_models(params,domain,cem,waves,set_land=True):
     cem.initialize(*args)
 
     
-    waves.set_value('sea_surface_water_wave__height', p['sea_surface_water_wave__height']);
-    waves.set_value('sea_surface_water_wave__period',p['sea_surface_water_wave__period']);
+    waves.set_value('sea_surface_water_wave__height', p['wave_height']);
+    waves.set_value('sea_surface_water_wave__period',p['wave_period']);
     waves.set_value('sea_shoreline_wave~incoming~deepwater__ashton_et_al_approach_angle_highness_parameter',
-                   p['sea_shoreline_wave~incoming~deepwater__ashton_et_al_approach_angle_highness_parameter']);
+                   p['wave_angle_highness']);
     waves.set_value('sea_shoreline_wave~incoming~deepwater__ashton_et_al_approach_angle_asymmetry_parameter',
-                   p['sea_shoreline_wave~incoming~deepwater__ashton_et_al_approach_angle_asymmetry_parameter']);
+                   p['wave_angle_asymmetry']);
 
     if set_land==True: #if need set land elevation; 'False' used default
         cem.set_value('land_surface__elevation',domain.flatten());
